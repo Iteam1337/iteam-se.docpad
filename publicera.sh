67162e59daf0fd74eccce235c106bd1a807f4e71
sophiewesslau <sophie.wesslau@iteam.se>
2014-02-20 12:00:53 +0100

    ändra i publicradokunetet

--- publicera.sh
+++ publicera.sh
@@ -4,5 +4,4 @@ KEY=AKIAIAKDS7MEDH4MTJIA
 SECRET=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
 # Show notifications and start deploy, finish with another notification
 osascript -e 'display notification "Startar publicering" with title "Iteam.se"'
-AWS_ACCESS_KEY_ID=KEY AWS_SECRET_ACCESS_KEY=SECRET grunt deploy:production
-osascript -e 'display notification "Publicerat!" with title "Iteam.se"'
+AWS_ACCESS_KEY_ID=AKIAIAKDS7MEDH4MTJIA AWS_SECRET_ACCESS_KEY=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 grunt deploy:production 'display notification "Publicerat!" with title "Iteam.se"'
