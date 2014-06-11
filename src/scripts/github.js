da2df3706873d8a9b9e1246f9cbb217471ec2c66
Rickard Laurin <rickard.laurin@gmail.com>
2014-06-11 11:33:53 +0200

    fixes #159, styling fixes, new sub menu, call-to-action jade include

---
+++ src/scripts/github.js
@@ -0,0 +1,52 @@
+(function () {
+
+  'use strict';
+
+  // Get GitHub element and reset xmlHttp from Instagram request
+  var github = document.getElementById('github');
+  var xmlHttp = null;
+
+  function ProcessGitHub () {
+    if (xmlHttp.readyState !== 4 || xmlHttp.status !== 200) { return; }
+
+    var res = JSON.parse(xmlHttp.responseText);
+    var list = document.createElement('ul');
+
+    res.forEach(function (data) {
+      var repo = document.createElement('li')
+      ,   a    = document.createElement('a')
+      ,   p    = document.createElement('p');
+
+      a.src = data.html_url;
+      a.innerHTML = data.name;
+
+      p.innerHTML = data.description;
+
+      repo.appendChild(a);
+      repo.appendChild(p);
+      list.appendChild(repo);
+    });
+
+
+    github.appendChild(list);
+  }
+
+  function readRSS (username) {
+    var url = 'https://api.github.com/users/{user}/repos';
+    url = url.replace('{user}', username);
+
+    console.log(url);
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = ProcessGitHub;
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+  }
+
+  if (!!github) {
+    var username = github.getAttribute('username');
+
+    readRSS(username);
+  }
+
+})();
