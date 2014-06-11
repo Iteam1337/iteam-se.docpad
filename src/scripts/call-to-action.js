becd2f44c351e5e5b7a93459d522169f6aa8f676
ilix <me@ilix.se>
2014-06-11 19:32:44 +0200

    Call to action now also posts to Slack.

--- src/scripts/call-to-action.js
+++ src/scripts/call-to-action.js
@@ -2,12 +2,31 @@
 
   'use strict';
 
-  var callToAction = document.getElementById('call-to-action');
+  var callToActionButton = $('.call-to-action-form .btn-success');
 
-  if (!!callToAction) {
-    callToAction.addEventListener('mouseup', function (e) {
-      e.target.parentElement.classList.toggle('open');
+  callToActionButton.click(function() {
+
+    var contactInfo = $( "[name*='entry.589797910']" )[0].value;
+    var contactMessage = $( "[name*='entry.1147522160']" )[0].value;
+
+    var payload = {
+      "username": "Call to action",
+      "text": 'Någon har fyllt i kontaktformuläret på iteam.se!\n\n' + contactMessage + '\n' + contactInfo,
+      "icon_emoji": ":footprints:"
+    };
+
+    $.ajax({
+      type: "POST",
+      url: 'https://iteamsolutions.slack.com/services/hooks/incoming-webhook?token=knBVNH7fnjAr49RzsIfKOplc',
+      data: JSON.stringify(payload),
+      success: function(data) {
+        // No action.
+      },
+      error: function(data) {
+        // No action.
+        // console.log('err', data.responseText);
+      },
+      dataType: 'json'
     });
-  }
-
+  });
 })();
