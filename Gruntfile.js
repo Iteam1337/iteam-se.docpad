7a9f4f7db1431dd0ba4220afb312e2ed97f05be4
Johanna Gustafsson <johanna.gustafsson@iteam.se>
2015-05-12 10:46:45 +0200

    Changed to sync for all builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,11 +84,12 @@ module.exports = function (grunt) {
         options: {
           bucket: 'www.iteam.se',
         },
-        upload: [
+        sync: [
           {
             src: 'out/**/*',
             dest: '/',
-            rel: 'out'
+            rel: 'out',
+            options: { verify: true }
           },
         ],
       },
@@ -96,11 +97,12 @@ module.exports = function (grunt) {
         options: {
           bucket: 'stage.iteam.se'
         },
-        upload: [
+        sync: [
           {
             src: 'out/**/*',
             dest: '/',
-            rel: 'out'
+            rel: 'out',
+            options: { verify: true }
           },
         ],
       },
